dc36766f97cd69cff080b7bb6b257d16ddc92150
keyontarver11 <ktarver@techlaunch.io>
2018-10-26 16:23:13 -0400

    first commit

---
+++ src/cardData.js
@@ -0,0 +1,13 @@
+const cardData = {
+  headline: 'Big Joe Blows Bubble Gum',
+
+  name: 'Joe Blow',
+
+  message: "Tired of bubble gum running out of flavor get some Joe Blow",
+
+  url: 'www.BigBlowJoe.com',
+
+  urlDisplayName: 'BlowJoe.com',
+}
+
+export default cardData;
